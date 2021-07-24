282e9a6bca3f91d0947a98f23c497720470cdf7d
mak7imyss <mak7imyss@mail.ru>
2021-07-24 12:13:50 +0300

    Задание 1. Автоматизация тестирования Api

--- tests/hm1.ts
+++ tests/hm1.ts
@@ -4,7 +4,7 @@ import { Cat } from '../@types/common';
 import { allure } from 'allure-mocha/runtime';
 
 const getRandomInt = (max: number) => Math.floor(Math.random() * max);
-let actCat: Cat;
+let expectedCat: Cat;
 
 describe('[HM] Поиск и удаление кота', async () => {
   before(() => {
@@ -15,45 +15,42 @@ describe('[HM] Поиск и удаление кота', async () => {
   });
   afterEach(() => {
     console.log('Завершение теста!');
+    console.info('♺', 'Убираем за собой, возвращая кота');
+    CoreApi.addCat([{ name: expectedCat.name, description: expectedCat.description, gender: expectedCat.gender }]);
   });
   after(() => {
     console.log('Завершение тестирования, ознакомьтесь с отчетом');
   });
 
   it('1. Поиск случайного кота', async () => {
+    const status: number = 200;
     console.info('Тест 1 ☑', 'Запрашиваем список котов сгруппированный по группам с 1 котом в группе');
 
     await allure.step('Находим случайного кота',
       async () => {
-        console.warn('Тест 1 ☑', 'Выполняем GET запрос /allByLetter');
-        const response = await CoreApi.allByLetter(1);
+        console.warn('Тест 1 ☑', 'Выполняем GET запрос /allByLetter для поиска группы котов');
+        const response = await CoreApi.getAllCarsByLetter(1);
         if (response.status === 404) {
           console.error('Тест 1 ☒', 'Ошибка выполнения GET запроса /allByLetter');
           assert.fail(`Кот не найден! Response:\n ${JSON.stringify(response.data, null, 2)}`);
         }
-
         console.debug('Тест 1 ☑', 'Получаем ответ на GET запрос /allByLetter');
-        console.info('Тест 1 ☑', 'Случайным образом выбираем кота из списка');
+        console.info('Тест 1 ☑', 'Случайным образом выбираем кота из полученного списка');
         const num = getRandomInt(response.data.groups.length);
         console.info('Тест 1 ☑', 'Получаем информацию о случайно выбранном коте');
-        actCat = response.data.groups[num].cats[0];
+        expectedCat = response.data.groups[num].cats[0];
         allure.testAttachment(
           'Информация о коте',
-          JSON.stringify(actCat, null, 2),
+          JSON.stringify(expectedCat, null, 2),
           'application/json');
 
         assert.ok(response.status === 200, `Текущий статус код ${response.status}`);
       },
     );
-  });
-
-  it('2. Удаление случайно найденного кота', async () => {
-    const status: number = 200;
-
-    await allure.step(`Удаление кота: ${actCat.name}`,
+    await allure.step(`Удаление кота: ${expectedCat.name}`,
       async () => {
-        console.warn('Тест 2 ☑', 'Выполняем DELETE запрос /remove');
-        const response = await CoreApi.removeCat(actCat.id);
+        console.warn('Тест 2 ☑', 'Выполняем DELETE запрос /remove для удаления кота');
+        const response = await CoreApi.removeCat(expectedCat.id);
         console.debug('Тест 2 ☑', 'Получаем ответ на DELETE запрос /remove');
         allure.testAttachment(
           'Информация об удаленном коте',
@@ -62,21 +59,20 @@ describe('[HM] Поиск и удаление кота', async () => {
         );
         assert.ok(response.status === status, `Текущий статус код ${response.status}`,
         );
-      });
-  });
-
-  it('3. Проверка что кота больше нет ', async () => {
-    await allure.step(`Удаление удаленного кота с id ${actCat.id}`,
+      },
+    );
+    await allure.step(`Проверка что кот: ${expectedCat.id} удален`,
       async () => {
-        console.warn('Тест 3 ☑', 'Выполняем DELETE запрос /remove');
-        const response = await CoreApi.removeCat(actCat.id);
-        console.debug('Тест 3 ☑', 'Получаем ответ на DELETE запрос /remove');
+        console.warn('Тест 3 ☑', 'Выполняем GET запрос /get-by-id с данными удаленного кота');
+        const response = await CoreApi.getCatById(expectedCat.id);
+        console.debug('Тест 3 ☑', 'Получаем ответ на GET запрос /get-by-id');
         allure.testAttachment(
           'Информация о запросе',
           JSON.stringify(response.data, null, 2),
           'application/json',
         );
-        assert.equal(response.data['output'].payload['message'], `Кот с id=${actCat.id} не найден`, 'Кот не удален');
+        assert.equal(response.data['output'].payload['message'], `Кот с id=${expectedCat.id} не найден`, 'Кот не удален');
       });
+
   });
 });
